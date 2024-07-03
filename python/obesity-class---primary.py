c032aef50ca32fe0fafa6098daec7ab007704c48
Martin Chapman <contact@martinchapman.co.uk>
2024-07-03 21:34:59 +0100

    Update made by Phenoflow

---
+++ python/obesity-class---primary.py
@@ -0,0 +1,25 @@
+# South East London (SEL) Long COVID Programme, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"914721000000105","system":"snomedct"},{"code":"914731000000107","system":"snomedct"},{"code":"914741000000103","system":"snomedct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('obesity-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["obesity-class---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["obesity-class---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["obesity-class---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
